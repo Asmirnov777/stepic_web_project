d0ead07996e3c89ea38d9156646cd6d1b4f9fc43
Asmirnov777 <101740775+Asmirnov777@users.noreply.github.com>
2022-07-19 11:14:45 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
+sudo rm /etc/nginx/sites-enabled/default #2> /dev/null
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+gunicorn --bind='0.0.0.0:8080' hello:divide_params -D
 sudo /etc/init.d/nginx restart
