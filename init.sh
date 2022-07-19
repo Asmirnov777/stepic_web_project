6bb18b4b0857ca7f472b0171777832bbd322cb87
Asmirnov777 <101740775+Asmirnov777@users.noreply.github.com>
2022-07-19 23:35:35 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 sudo rm /etc/nginx/sites-enabled/default #2> /dev/null
-sudo ln -sf ./etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 gunicorn --bind='0.0.0.0:8080' hello:divide_params -D
-gunicorn -c ./etc/qa_conf.py ask.wsgi:application -D
+gunicorn -c /home/box/web/etc/qa_conf.py ask.wsgi:application -D
 sudo /etc/init.d/nginx restart
 
-#git clone https://github.com/<your_account>/stepic_web_2_1 /home/box/web
+#sudo pip3 install django==2.0
+#git clone https://github.com/<your_account>/stepic_web_project /home/box/web
+#cd /home/box/web/ask/
 #bash /home/box/web/init.sh
