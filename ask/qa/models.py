c2e7dea1767b2f7e9944f4809db5cc2f364e296e
Asmirnov777 <101740775+Asmirnov777@users.noreply.github.com>
2022-07-22 21:23:52 +0300

    Add files via upload

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -11,8 +11,8 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateField(blank = True, auto_now_add=True)
+    rating = models.IntegerField(default = 0)
     author = models.ForeignKey(User, related_name = 'author', on_delete = models.DO_NOTHING)
     likes = models.ManyToManyField(User, related_name = 'likes')
     objects = QuestionManager()
